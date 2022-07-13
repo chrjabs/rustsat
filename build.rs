d92156e9dd58e172b4187f161fb17830d6665363
Christoph Jabs <christoph.jabs@helsinki.fi>
2022-07-13 15:48:23 +0300

    rework cadical build

--- build.rs
+++ build.rs
@@ -1,18 +1,31 @@
+use std::path::Path;
 use std::process::Command;
 
 fn main() {
     #[cfg(feature = "cadical")]
     {
-        // TODO: Only configure if necessary
-        Command::new("./configure")
-            .current_dir("solvers/cadical")
+        let cadical_path = "solvers/cadical";
+        println!("cargo:rerun-if-changed={}", cadical_path);
+        Command::new("git")
+            .arg("submodule")
+            .arg("update")
+            .arg("--init")
+            .arg(cadical_path)
             .output()
-            .expect("failed to run CaDiCal configure script");
+            .expect("failed to update CaDiCaL submodule");
+        if !Path::new(&format!("{}/makefile", cadical_path)).exists() {
+            // Only configure if makefile doesn't exist
+            Command::new("./configure")
+                .current_dir(cadical_path)
+                .output()
+                .expect("failed to run CaDiCal configure script");
+        }
         Command::new("make")
-            .current_dir("solvers/cadical")
+            .arg("-j")
+            .current_dir(cadical_path)
             .output()
             .expect("failed to make CaDiCaL");
-        println!("cargo:rustc-link-search=native=solvers/cadical/build");
+        println!("cargo:rustc-link-search=native={}/build", cadical_path);
         println!("cargo:rustc-link-lib=static=cadical");
 
         #[cfg(target_os = "macos")]
