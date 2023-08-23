bcd6e4f72159163e9cbb431faf45e1739e88e7bf
Christoph Jabs <christoph.jabs@helsinki.fi>
2023-08-23 15:45:54 +0300

    formatting

--- tools/src/bin/opb2wcnf.rs
+++ tools/src/bin/opb2wcnf.rs
@@ -20,7 +20,10 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    let opb_opts = OpbOptions { first_var_idx: 0, ..Default::default() };
+    let opb_opts = OpbOptions {
+        first_var_idx: 0,
+        ..Default::default()
+    };
 
     let inst: OptInstance = if let Some(in_path) = args.in_path {
         OptInstance::from_opb_path(in_path, opb_opts).expect("error parsing the input file")
