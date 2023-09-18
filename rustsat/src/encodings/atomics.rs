a31345bd786c220dcc91bb431866f8f33eaac943
Christoph Jabs <christoph.jabs@helsinki.fi>
2023-09-18 13:51:29 +0300

    more atomics

--- rustsat/src/encodings/atomics.rs
+++ rustsat/src/encodings/atomics.rs
@@ -30,3 +30,43 @@ pub fn cube_impl_clause(a: &[Lit], b: &[Lit]) -> Clause {
     cl.extend(b.iter().copied());
     cl
 }
+
+/// Implication of form `a -> (b1 & b2 & ... & bm)`
+pub fn lit_impl_cube(a: Lit, b: &[Lit]) -> impl Iterator<Item = Clause> + '_ {
+    b.iter().map(move |bi| clause![!a, *bi])
+}
+
+/// Implication of form `(a1 | a2 | ... | an) -> b`
+pub fn clause_impl_lit(a: &[Lit], b: Lit) -> impl Iterator<Item = Clause> + '_ {
+    a.iter().map(move |ai| clause![!*ai, b])
+}
+
+/// Implication of form `(a1 | a2 | ... | an) -> (b1 | b2 | ... | bm)`
+pub fn clause_impl_clause<'all>(
+    a: &'all [Lit],
+    b: &'all [Lit],
+) -> impl Iterator<Item = Clause> + 'all {
+    a.iter().map(move |ai| {
+        let mut cl: Clause = b.iter().copied().collect();
+        cl.add(!*ai);
+        cl
+    })
+}
+
+/// Implication of form `(a1 | a2 | ... | an) -> (b1 & b2 & ... | bm)`
+pub fn clause_impl_cube<'all>(
+    a: &'all [Lit],
+    b: &'all [Lit],
+) -> impl Iterator<Item = Clause> + 'all {
+    a.iter()
+        .flat_map(move |ai| b.iter().map(|bi| clause![!*ai, *bi]))
+}
+
+/// Implication of form `(a1 & a2 & ... & an) -> (b1 & b2 & ... & bm)`
+pub fn cube_impl_cube<'all>(a: &'all [Lit], b: &'all [Lit]) -> impl Iterator<Item = Clause> + 'all {
+    b.iter().map(move |bi| {
+        let mut cl: Clause = a.iter().copied().map(std::ops::Not::not).collect();
+        cl.add(*bi);
+        cl
+    })
+}
