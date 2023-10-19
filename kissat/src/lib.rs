dc162b42d6259b9ffb9f5b38aa9a036a2d516890
Christoph Jabs <christoph.jabs@helsinki.fi>
2023-10-19 13:28:26 +0300

    adapt to changes in traits

--- kissat/src/lib.rs
+++ kissat/src/lib.rs
@@ -355,7 +355,7 @@ unsafe impl Send for Interrupter {}
 unsafe impl Sync for Interrupter {}
 
 impl InterruptSolver for Interrupter {
-    fn interrupt(&mut self) {
+    fn interrupt(&self) {
         unsafe { ffi::kissat_terminate(self.handle) }
     }
 }
