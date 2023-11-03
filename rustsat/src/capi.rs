3d84511e7a1ae638bdba501ab16195835b6de23f
Christoph Jabs <christoph.jabs@helsinki.fi>
2023-11-03 12:36:48 +0200

    one more capi test

--- rustsat/src/capi.rs
+++ rustsat/src/capi.rs
@@ -469,6 +469,46 @@ pub mod encodings {
                 })
                 .success();
             }
+
+            #[test]
+            fn coarse_convergence() {
+                (assert_c! {
+                    #include <assert.h>
+                    #include "rustsat.h"
+
+                    void assump_counter(int lit, void *data) {
+                        assert(lit);
+                        int *cnt = (int *) data;
+                        (*cnt)++;
+                    }
+
+                    void clause_counter(int lit, void *data) {
+                        if (!lit) {
+                            int *cnt = (int *)data;
+                            (*cnt)++;
+                        }
+                    }
+
+                    int main() {
+                        DynamicPolyWatchdog *dpw = dpw_new();
+                        dpw_add(dpw, 1, 5);
+                        dpw_add(dpw, 2, 3);
+                        dpw_add(dpw, 3, 8);
+                        dpw_add(dpw, 4, 7);
+                        int n_used = 4;
+                        int n_clauses = 0;
+                        dpw_encode_ub(dpw, 0, 23, &n_used, &clause_counter, &n_clauses);
+                        for (size_t ub = 8; ub <= 23; ub++) {
+                            size_t coarse_ub = dpw_coarse_ub(dpw, ub);
+                            assert(coarse_ub <= ub);
+                            int n_assumps = 0;
+                            assert(dpw_enforce_ub(dpw, coarse_ub, &assump_counter, &n_assumps) == Ok);
+                            assert(n_assumps == 1);
+                        }
+                    }
+                })
+                .success();
+            }
         }
     }
 }
