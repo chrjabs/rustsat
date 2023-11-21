3f7972d8c9021f948d95929acafae694114ef428
Christoph Jabs <christoph.jabs@helsinki.fi>
2023-11-21 20:32:53 +0200

    docs: (limited) python api documentation

--- rustsat/src/pyapi.rs
+++ rustsat/src/pyapi.rs
@@ -3,22 +3,53 @@
 //! This is the Python API for RustSAT. Currently this API is very minimal and
 //! not the focus of this project. For now, only the API of certain encodings is
 //! available.
+//!
+//! ## Classes
+//!
+//! The following classes are available as Python bindings.
+//!
+//! ```bash
+//! rustsat
+//! ├── Clause
+//! ├── Cnf
+//! ├── Lit
+//! ├── VarManager
+//! └── encodings
+//!     ├── DynamicPolyWatchdog
+//!     ├── GeneralizedTotalizer
+//!     └── Totalizer
+//! ```
+//!
+//! They have similar APIs (but reduced functionality) to the following Rust types:
+//!
+//! | Python Class | Rust Type |
+//! | --- | --- |
+//! | `rustsat.Clause` | [`crate::types::Clause`] |
+//! | `rustsat.Cnf` | [`crate::instances::Cnf`] |
+//! | `rustsat.Lit` | [`crate::types::Lit`] |
+//! | `rustsat.VarManager` | [`crate::instances::BasicVarManager`] |
+//! | `rustsat.encodings.DynamicPolyWatchdog` | [`crate::encodings::pb::DynamicPolyWatchdog`] |
+//! | `rustsat.encodings.GeneralizedTotalizer` | [`crate::encodings::pb::GeneralizedTotalizer`] |
+//! | `rustsat.encodings.Totalizer` | [`crate::encodings::card::Totalizer`] |
 
 use pyo3::{prelude::*, types::PySlice};
 
 use crate::{
-    encodings::{card::DbTotalizer, pb::{DbGte, DynamicPolyWatchdog}},
+    encodings::{
+        card::DbTotalizer,
+        pb::{DbGte, DynamicPolyWatchdog},
+    },
     instances::{BasicVarManager, Cnf},
     types::{Clause, Lit},
 };
 
 #[derive(FromPyObject)]
-pub enum SliceOrInt<'a> {
+pub(crate) enum SliceOrInt<'a> {
     Slice(&'a PySlice),
     Int(isize),
 }
 
-pub enum SingleOrList<T> {
+pub(crate) enum SingleOrList<T> {
     Single(T),
     List(Vec<T>),
 }
