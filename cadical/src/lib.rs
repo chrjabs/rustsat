72af2e02102bbc4b5265490de0a884e97573ef6b
Christoph Jabs <christoph.jabs@helsinki.fi>
2024-01-11 10:42:07 +0200

    formatting

--- cadical/src/lib.rs
+++ cadical/src/lib.rs
@@ -1,22 +1,22 @@
 //! # rustsat-cadical - Interface to the CaDiCaL SAT Solver for RustSAT
-//! 
+//!
 //! Armin Biere's SAT solver [CaDiCaL](https://github.com/arminbiere/cadical) to be used with the [RustSAT](https://github.com/chrjabs/rustsat) library.
-//! 
+//!
 //! **Note**: at the moment this crate is known to not work on Windows since CaDiCaL is non-trivial to get to work on Windows.
-//! 
+//!
 //! ## Features
-//! 
+//!
 //! - `debug`: if this feature is enables, the C++ library will be built with debug and check functionality if the Rust project is built in debug mode
 //! - `safe`: disable writing through 'popen' for more safe usage of the library in applications
 //! - `quiet`: exclude message and profiling code (logging too)
 //! - `logging`: include logging code (but disabled by default)
-//! 
+//!
 //! ## CaDiCaL Versions
-//! 
+//!
 //! CaDiCaL versions can be selected via cargo crate features.
 //! All CaDiCaL versions up to [Version 1.9.4](https://github.com/arminbiere/cadical/releases/tag/rel-1.9.4) are available.
 //! For the full list of versions and the changelog see [the CaDiCaL releases](https://github.com/arminbiere/cadical/releases).
-//! 
+//!
 //! Without any features selected, the newest version will be used.
 //! If conflicting CaDiCaL versions are requested, the newest requested version will be selected.
 
--- kissat/src/lib.rs
+++ kissat/src/lib.rs
@@ -1,17 +1,17 @@
 //! # rustsat-kissat - Interface to the kissat SAT Solver for RustSAT
-//! 
+//!
 //! Armin Biere's SAT solver [Kissat](https://github.com/arminbiere/kissat) to be used with the [RustSAT](https://github.com/chrjabs/rustsat) library.
-//! 
+//!
 //! **Note**: at the moment this crate is known to not work on Windows since Kissat is non-trivial to get to work on Windows.
-//! 
+//!
 //! ## Features
-//! 
+//!
 //! - `debug`: if this feature is enables, the C library will be built with debug functionality if the Rust project is built in debug mode
 //! - `safe`: disable writing through 'popen' for more safe usage of the library in applications
 //! - `quiet`: exclude message and profiling code (logging too)
-//! 
+//!
 //! ## Kissat Versions
-//! 
+//!
 //! Kissat versions can be selected via cargo crate features.
 //! The following Kissat versions are available:
 //! - `v3-1-0`: [Version 3.1.0](https://github.com/arminbiere/kissat/releases/tag/rel-3.1.0)
@@ -19,7 +19,7 @@
 //! - `sc2022-light`: [SAT Competition 2022 Light](https://github.com/arminbiere/kissat/releases/tag/sc2022-light)
 //! - `sc2022-hyper`: [SAT Competition 2022 Hyper](https://github.com/arminbiere/kissat/releases/tag/sc2022-hyper)
 //! - `sc2022-bulky`: [SAT Competition 2022 Bulky](https://github.com/arminbiere/kissat/releases/tag/sc2022-bulky)
-//! 
+//!
 //! Without any features selected, the newest version will be used.
 //! If conflicting Kissat versions are requested, the newest requested version will be selected.
 
