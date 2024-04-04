7674edbac6d20930f9ffa22b0c086a0d9d56c42b
Christoph Jabs <christoph.jabs@helsinki.fi>
2024-04-04 11:43:09 +0300

    refactor: factor out solver integration tests

--- kissat/tests/small.rs
+++ kissat/tests/small.rs
@@ -1,102 +1,20 @@
-use rustsat::{
-    instances::{BasicVarManager, SatInstance},
-    solvers::{Solve, SolverResult},
-};
-use rustsat_kissat::Kissat;
-
-fn small_sat_instance<S: Solve>(mut solver: S) {
-    let inst: SatInstance<BasicVarManager> =
-        SatInstance::from_dimacs_path("./data/AProVE11-12.cnf").unwrap();
-    solver.add_cnf(inst.as_cnf().0).unwrap();
-    let res = solver.solve().unwrap();
-    assert_eq!(res, SolverResult::Sat);
+mod base {
+    rustsat_solvertests::base_tests!(rustsat_kissat::Kissat);
 }
 
-fn small_unsat_instance<S: Solve>(mut solver: S) {
-    let inst: SatInstance<BasicVarManager> =
-        SatInstance::from_dimacs_path("./data/smtlib-qfbv-aigs-ext_con_032_008_0256-tseitin.cnf")
+mod sat {
+    rustsat_solvertests::base_tests!({
+        let mut slv = rustsat_kissat::Kissat::default();
+        slv.set_configuration(rustsat_kissat::Config::SAT).unwrap();
+        slv
+    });
+}
+
+mod unsat {
+    rustsat_solvertests::base_tests!({
+        let mut slv = rustsat_kissat::Kissat::default();
+        slv.set_configuration(rustsat_kissat::Config::UNSAT)
             .unwrap();
-    solver.add_cnf(inst.as_cnf().0).unwrap();
-    let res = solver.solve().unwrap();
-    assert_eq!(res, SolverResult::Unsat);
-}
-
-fn ms_segfault_instance<S: Solve>(mut solver: S) {
-    let inst: SatInstance<BasicVarManager> =
-        SatInstance::from_dimacs_path("./data/minisat-segfault.cnf").unwrap();
-    solver.add_cnf(inst.as_cnf().0).unwrap();
-    let res = solver.solve().unwrap();
-    assert_eq!(res, SolverResult::Unsat);
-}
-
-#[test]
-fn small_sat() {
-    let solver = Kissat::default();
-    small_sat_instance(solver);
-}
-
-#[test]
-fn small_unsat() {
-    let solver = Kissat::default();
-    small_unsat_instance(solver);
-}
-
-#[test]
-fn ms_segfault() {
-    let solver = Kissat::default();
-    ms_segfault_instance(solver);
-}
-
-#[test]
-fn sat_small_sat() {
-    let mut solver = Kissat::default();
-    solver
-        .set_configuration(rustsat_kissat::Config::SAT)
-        .unwrap();
-    small_sat_instance(solver);
-}
-
-#[test]
-fn sat_small_unsat() {
-    let mut solver = Kissat::default();
-    solver
-        .set_configuration(rustsat_kissat::Config::SAT)
-        .unwrap();
-    small_unsat_instance(solver);
-}
-
-#[test]
-fn sat_ms_segfault() {
-    let mut solver = Kissat::default();
-    solver
-        .set_configuration(rustsat_kissat::Config::SAT)
-        .unwrap();
-    ms_segfault_instance(solver);
-}
-
-#[test]
-fn unsat_small_sat() {
-    let mut solver = Kissat::default();
-    solver
-        .set_configuration(rustsat_kissat::Config::UNSAT)
-        .unwrap();
-    small_sat_instance(solver);
-}
-
-#[test]
-fn unsat_small_unsat() {
-    let mut solver = Kissat::default();
-    solver
-        .set_configuration(rustsat_kissat::Config::UNSAT)
-        .unwrap();
-    small_unsat_instance(solver);
-}
-
-#[test]
-fn unsat_ms_segfault() {
-    let mut solver = Kissat::default();
-    solver
-        .set_configuration(rustsat_kissat::Config::UNSAT)
-        .unwrap();
-    ms_segfault_instance(solver);
+        slv
+    });
 }
--- minisat/tests/small.rs
+++ minisat/tests/small.rs
@@ -1,71 +1,7 @@
-use rustsat::{
-    instances::{BasicVarManager, SatInstance},
-    solvers::{Solve, SolverResult},
-};
-use rustsat_minisat::{core, simp};
-
-#[test]
-fn core_small_sat() {
-    let mut solver = core::Minisat::default();
-    let inst: SatInstance<BasicVarManager> =
-        SatInstance::from_dimacs_path("./data/AProVE11-12.cnf").unwrap();
-    solver.add_cnf(inst.as_cnf().0).unwrap();
-    let res = solver.solve().unwrap();
-    assert_eq!(res, SolverResult::Sat);
+mod core {
+    rustsat_solvertests::base_tests!(rustsat_minisat::core::Minisat, false, true);
 }
 
-// Note: this instance seems too hard for minisat to solve
-#[test]
-#[ignore]
-fn core_small_unsat() {
-    let mut solver = core::Minisat::default();
-    let inst: SatInstance<BasicVarManager> =
-        SatInstance::from_dimacs_path("./data/smtlib-qfbv-aigs-ext_con_032_008_0256-tseitin.cnf")
-            .unwrap();
-    solver.add_cnf(inst.as_cnf().0).unwrap();
-    let res = solver.solve().unwrap();
-    assert_eq!(res, SolverResult::Unsat);
-}
-
-#[test]
-fn core_ms_segfault() {
-    let mut solver = core::Minisat::default();
-    let inst: SatInstance<BasicVarManager> =
-        SatInstance::from_dimacs_path("./data/minisat-segfault.cnf").unwrap();
-    solver.add_cnf(inst.as_cnf().0).unwrap();
-    let res = solver.solve().unwrap();
-    assert_eq!(res, SolverResult::Unsat);
-}
-
-#[test]
-fn simp_small_sat() {
-    let mut solver = simp::Minisat::default();
-    let inst: SatInstance<BasicVarManager> =
-        SatInstance::from_dimacs_path("./data/AProVE11-12.cnf").unwrap();
-    solver.add_cnf(inst.as_cnf().0).unwrap();
-    let res = solver.solve().unwrap();
-    assert_eq!(res, SolverResult::Sat);
-}
-
-// Note: this instance seems too hard for minisat to solve
-#[test]
-#[ignore]
-fn simp_small_unsat() {
-    let mut solver = simp::Minisat::default();
-    let inst: SatInstance<BasicVarManager> =
-        SatInstance::from_dimacs_path("./data/smtlib-qfbv-aigs-ext_con_032_008_0256-tseitin.cnf")
-            .unwrap();
-    solver.add_cnf(inst.as_cnf().0).unwrap();
-    let res = solver.solve().unwrap();
-    assert_eq!(res, SolverResult::Unsat);
-}
-
-#[test]
-fn simp_ms_segfault() {
-    let mut solver = simp::Minisat::default();
-    let inst: SatInstance<BasicVarManager> =
-        SatInstance::from_dimacs_path("./data/minisat-segfault.cnf").unwrap();
-    solver.add_cnf(inst.as_cnf().0).unwrap();
-    let res = solver.solve().unwrap();
-    assert_eq!(res, SolverResult::Unsat);
+mod simp {
+    rustsat_solvertests::base_tests!(rustsat_minisat::simp::Minisat, false, true);
 }
