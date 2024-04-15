af9d3fc783eb70d503b65f0f4169c10f32aa418e
Christoph Jabs <christoph.jabs@helsinki.fi>
2024-04-15 14:57:15 +0300

    feat: `Extend<&Clause>` for solvers

--- kissat/src/lib.rs
+++ kissat/src/lib.rs
@@ -188,6 +188,15 @@ impl Extend<Clause> for Kissat<'_> {
     }
 }
 
+impl<'a> Extend<&'a Clause> for Kissat<'_> {
+    fn extend<T: IntoIterator<Item = &'a Clause>>(&mut self, iter: T) {
+        iter.into_iter().for_each(|cl| {
+            self.add_clause_ref(cl)
+                .expect("Error adding clause in extend")
+        })
+    }
+}
+
 impl Solve for Kissat<'_> {
     fn signature(&self) -> &'static str {
         let c_chars = unsafe { ffi::kissat_signature() };
