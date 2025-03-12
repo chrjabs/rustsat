32d3a4f6332863efb5d660238b1fbd579d9db41e
Christoph Jabs <christoph.jabs@helsinki.fi>
2025-03-12 13:06:19 +0200

    chore: force synced versions via workspace

--- scripts/sync-versions.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-VERSION=$(grep '^version = "[[:digit:]]\+.[[:digit:]]\+.[[:digit:]]\+"' Cargo.toml | cut -d '"' -f2)
-SED_PATTERN="s/^version = \"[[:digit:]]\+.[[:digit:]]\+.[[:digit:]]\+\"$/version = \"${VERSION}\"/g"
-
-# sync C-API version
-sed -e "${SED_PATTERN}" -i capi/Cargo.toml
-# sync Python API version
-sed -e "${SED_PATTERN}" -i pyapi/Cargo.toml
