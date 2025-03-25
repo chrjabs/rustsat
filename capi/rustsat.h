896129d9ce185adb86af645f8e003db82ded9e52
Christoph Jabs <christoph.jabs@helsinki.fi>
2025-03-25 10:36:58 +0200

    chore(capi): don't sort functions

--- capi/rustsat.h
+++ capi/rustsat.h
@@ -114,6 +114,11 @@ extern "C" {
 #endif // __cplusplus
 
 /**
+ * Creates a new [`BinaryAdder`] cardinality encoding
+ */
+struct BinaryAdder *bin_adder_new(void);
+
+/**
  * Adds a new input literal to a [`BinaryAdder`].
  *
  * # Errors
@@ -127,46 +132,6 @@ extern "C" {
  * called on.
  */
 enum MaybeError bin_adder_add(struct BinaryAdder *adder, int lit, size_t weight);
-
-/**
- * Frees the memory associated with a [`BinaryAdder`]
- *
- * # Safety
- *
- * `adder` must be a return value of [`bin_adder_new`] and cannot be used
- * afterwards again.
- */
-void bin_adder_drop(struct BinaryAdder *adder);
-
-/**
- * Lazily builds the _change in_ pseudo-boolean encoding to enable lower bounds from within the
- * range.
- *
- * The min and max bounds are inclusive. After a call to [`bin_adder_encode_lb`] with
- * `min_bound=2` and `max_bound=4`, bounds satisfying `2 <= bound <= 4` can be enforced.
- *
- * Clauses are returned via the `collector`. The `collector` function should expect clauses to be
- * passed similarly to `ipasir_add`, as a 0-terminated sequence of literals where the literals are
- * passed as the first argument and the `collector_data` as a second.
- *
- * `n_vars_used` must be the number of variables already used and will be incremented by the
- * number of variables used up in the encoding.
- *
- * # Safety
- *
- * `adder` must be a return value of [`bin_adder_new`] that [`bin_adder_drop`] has not yet been
- * called on.
- *
- * # Panics
- *
- * If `min_bound > max_bound`.
- */
-void bin_adder_encode_lb(struct BinaryAdder *adder,
-                         size_t min_bound,
-                         size_t max_bound,
-                         uint32_t *n_vars_used,
-                         CClauseCollector collector,
-                         void *collector_data);
 
 /**
  * Lazily builds the _change in_ pseudo-boolean encoding to enable upper bounds from within the
@@ -200,24 +165,6 @@ void bin_adder_encode_ub(struct BinaryAdder *adder,
                          void *collector_data);
 
 /**
- * Returns assumptions/units for enforcing an upper bound (`sum of lits >= lb`). Make sure that
- * [`bin_adder_encode_lb`] has been called adequately and nothing has been called afterwards,
- * otherwise [`MaybeError::NotEncoded`] will be returned.
- *
- * Assumptions are returned via the collector callback. There is _no_ terminating zero, all
- * assumptions are passed when [`bin_adder_enforce_lb`] returns.
- *
- * # Safety
- *
- * `adder` must be a return value of [`bin_adder_new`] that [`bin_adder_drop`] has not yet been
- * called on.
- */
-enum MaybeError bin_adder_enforce_lb(struct BinaryAdder *adder,
-                                     size_t ub,
-                                     CAssumpCollector collector,
-                                     void *collector_data);
-
-/**
  * Returns assumptions/units for enforcing an upper bound (`sum of lits <= ub`). Make sure that
  * [`bin_adder_encode_ub`] has been called adequately and nothing has been called afterwards,
  * otherwise [`MaybeError::NotEncoded`] will be returned.
@@ -236,9 +183,52 @@ enum MaybeError bin_adder_enforce_ub(struct BinaryAdder *adder,
                                      void *collector_data);
 
 /**
- * Creates a new [`BinaryAdder`] cardinality encoding
+ * Lazily builds the _change in_ pseudo-boolean encoding to enable lower bounds from within the
+ * range.
+ *
+ * The min and max bounds are inclusive. After a call to [`bin_adder_encode_lb`] with
+ * `min_bound=2` and `max_bound=4`, bounds satisfying `2 <= bound <= 4` can be enforced.
+ *
+ * Clauses are returned via the `collector`. The `collector` function should expect clauses to be
+ * passed similarly to `ipasir_add`, as a 0-terminated sequence of literals where the literals are
+ * passed as the first argument and the `collector_data` as a second.
+ *
+ * `n_vars_used` must be the number of variables already used and will be incremented by the
+ * number of variables used up in the encoding.
+ *
+ * # Safety
+ *
+ * `adder` must be a return value of [`bin_adder_new`] that [`bin_adder_drop`] has not yet been
+ * called on.
+ *
+ * # Panics
+ *
+ * If `min_bound > max_bound`.
  */
-struct BinaryAdder *bin_adder_new(void);
+void bin_adder_encode_lb(struct BinaryAdder *adder,
+                         size_t min_bound,
+                         size_t max_bound,
+                         uint32_t *n_vars_used,
+                         CClauseCollector collector,
+                         void *collector_data);
+
+/**
+ * Returns assumptions/units for enforcing an upper bound (`sum of lits >= lb`). Make sure that
+ * [`bin_adder_encode_lb`] has been called adequately and nothing has been called afterwards,
+ * otherwise [`MaybeError::NotEncoded`] will be returned.
+ *
+ * Assumptions are returned via the collector callback. There is _no_ terminating zero, all
+ * assumptions are passed when [`bin_adder_enforce_lb`] returns.
+ *
+ * # Safety
+ *
+ * `adder` must be a return value of [`bin_adder_new`] that [`bin_adder_drop`] has not yet been
+ * called on.
+ */
+enum MaybeError bin_adder_enforce_lb(struct BinaryAdder *adder,
+                                     size_t ub,
+                                     CAssumpCollector collector,
+                                     void *collector_data);
 
 /**
  * Reserves all auxiliary variables that the encoding might need
@@ -255,6 +245,21 @@ struct BinaryAdder *bin_adder_new(void);
 void bin_adder_reserve(struct BinaryAdder *adder, uint32_t *n_vars_used);
 
 /**
+ * Frees the memory associated with a [`BinaryAdder`]
+ *
+ * # Safety
+ *
+ * `adder` must be a return value of [`bin_adder_new`] and cannot be used
+ * afterwards again.
+ */
+void bin_adder_drop(struct BinaryAdder *adder);
+
+/**
+ * Creates a new [`DynamicPolyWatchdog`] cardinality encoding
+ */
+struct DynamicPolyWatchdog *dpw_new(void);
+
+/**
  * Adds a new input literal to a [`DynamicPolyWatchdog`].
  *
  * # Errors
@@ -268,25 +273,6 @@ void bin_adder_reserve(struct BinaryAdder *adder, uint32_t *n_vars_used);
  * `dpw` must be a return value of [`dpw_new`] that [`dpw_drop`] has not yet been called on.
  */
 enum MaybeError dpw_add(struct DynamicPolyWatchdog *dpw, int lit, size_t weight);
-
-/**
- * Gets the next smaller upper bound value that can be encoded without setting tares. This is used
- * for coarse convergence.
- *
- * # Safety
- *
- * `dpw` must be a return value of [`dpw_new`] that [`dpw_drop`] has not yet been called on.
- */
-size_t dpw_coarse_ub(struct DynamicPolyWatchdog *dpw, size_t ub);
-
-/**
- * Frees the memory associated with a [`DynamicPolyWatchdog`]
- *
- * # Safety
- *
- * `dpw` must be a return value of [`dpw_new`] and cannot be used afterwards again.
- */
-void dpw_drop(struct DynamicPolyWatchdog *dpw);
 
 /**
  * Lazily builds the _change in_ pseudo-boolean encoding to enable upper bounds from within the
@@ -337,6 +323,49 @@ enum MaybeError dpw_enforce_ub(struct DynamicPolyWatchdog *dpw,
                                void *collector_data);
 
 /**
+ * Gets the next smaller upper bound value that can be encoded without setting tares. This is used
+ * for coarse convergence.
+ *
+ * # Safety
+ *
+ * `dpw` must be a return value of [`dpw_new`] that [`dpw_drop`] has not yet been called on.
+ */
+size_t dpw_coarse_ub(struct DynamicPolyWatchdog *dpw, size_t ub);
+
+/**
+ * Set the precision at which to build the encoding at. With `divisor = 8` the encoding will
+ * effectively be built such that the weight of every input literal is divided by `divisor`
+ * (integer division, rounding down). Divisor values must be powers of 2. After building the
+ * encoding, the precision can only be increased, i.e., only call this function with _decreasing_
+ * divisor values.
+ *
+ * # Errors
+ *
+ * - If `divisor` is not a power of 2, [`MaybeError::PrecisionNotPow2`] is returned
+ * - If `divisor` is larger than the last divisor, i.e., precision is attempted to be decreased,
+ *     [`MaybeError::PrecisionDecreased`] is returned
+ *
+ * # Safety
+ *
+ * `dpw` must be a return value of [`dpw_new`] that [`dpw_drop`] has not yet been called on.
+ */
+enum MaybeError dpw_set_precision(struct DynamicPolyWatchdog *dpw, size_t divisor);
+
+/**
+ * Gets the next possible precision divisor value
+ *
+ * Note that this is not the next possible precision value from the last _set_ precision but from
+ * the last _encoded_ precision. The divisor value will always be a power of two so that calling
+ * `set_precision` and then encoding will produce the smallest non-empty next segment of the
+ * encoding.
+ *
+ * # Safety
+ *
+ * `dpw` must be a return value of [`dpw_new`] that [`dpw_drop`] has not yet been called on.
+ */
+size_t dpw_next_precision(struct DynamicPolyWatchdog *dpw);
+
+/**
  * Checks whether the encoding is already at the maximum precision
  *
  * # Safety
@@ -380,25 +409,6 @@ void dpw_limit_range(struct DynamicPolyWatchdog *dpw,
                      void *collector_data);
 
 /**
- * Creates a new [`DynamicPolyWatchdog`] cardinality encoding
- */
-struct DynamicPolyWatchdog *dpw_new(void);
-
-/**
- * Gets the next possible precision divisor value
- *
- * Note that this is not the next possible precision value from the last _set_ precision but from
- * the last _encoded_ precision. The divisor value will always be a power of two so that calling
- * `set_precision` and then encoding will produce the smallest non-empty next segment of the
- * encoding.
- *
- * # Safety
- *
- * `dpw` must be a return value of [`dpw_new`] that [`dpw_drop`] has not yet been called on.
- */
-size_t dpw_next_precision(struct DynamicPolyWatchdog *dpw);
-
-/**
  * Reserves all auxiliary variables that the encoding might need
  *
  * All calls to [`dpw_encode_ub`] following a call to this function are guaranteed to not increase
@@ -411,23 +421,18 @@ size_t dpw_next_precision(struct DynamicPolyWatchdog *dpw);
 void dpw_reserve(struct DynamicPolyWatchdog *dpw, uint32_t *n_vars_used);
 
 /**
- * Set the precision at which to build the encoding at. With `divisor = 8` the encoding will
- * effectively be built such that the weight of every input literal is divided by `divisor`
- * (integer division, rounding down). Divisor values must be powers of 2. After building the
- * encoding, the precision can only be increased, i.e., only call this function with _decreasing_
- * divisor values.
- *
- * # Errors
- *
- * - If `divisor` is not a power of 2, [`MaybeError::PrecisionNotPow2`] is returned
- * - If `divisor` is larger than the last divisor, i.e., precision is attempted to be decreased,
- *     [`MaybeError::PrecisionDecreased`] is returned
+ * Frees the memory associated with a [`DynamicPolyWatchdog`]
  *
  * # Safety
  *
- * `dpw` must be a return value of [`dpw_new`] that [`dpw_drop`] has not yet been called on.
+ * `dpw` must be a return value of [`dpw_new`] and cannot be used afterwards again.
  */
-enum MaybeError dpw_set_precision(struct DynamicPolyWatchdog *dpw, size_t divisor);
+void dpw_drop(struct DynamicPolyWatchdog *dpw);
+
+/**
+ * Creates a new [`DbGte`] cardinality encoding
+ */
+struct DbGte *gte_new(void);
 
 /**
  * Adds a new input literal to a [`DbGte`].
@@ -442,16 +447,6 @@ enum MaybeError dpw_set_precision(struct DynamicPolyWatchdog *dpw, size_t diviso
  * `gte` must be a return value of [`gte_new`] that [`gte_drop`] has not yet been called on.
  */
 enum MaybeError gte_add(struct DbGte *gte, int lit, size_t weight);
-
-/**
- * Frees the memory associated with a [`DbGte`]
- *
- * # Safety
- *
- * `gte` must be a return value of [`gte_new`] and cannot be used
- * afterwards again.
- */
-void gte_drop(struct DbGte *gte);
 
 /**
  * Lazily builds the _change in_ pseudo-boolean encoding to enable upper bounds from within the
@@ -501,11 +496,6 @@ enum MaybeError gte_enforce_ub(struct DbGte *gte,
                                void *collector_data);
 
 /**
- * Creates a new [`DbGte`] cardinality encoding
- */
-struct DbGte *gte_new(void);
-
-/**
  * Reserves all auxiliary variables that the encoding might need
  *
  * All calls to [`gte_encode_ub`] following a call to this function are guaranteed to not increase
@@ -516,6 +506,21 @@ struct DbGte *gte_new(void);
  * `gte` must be a return value of [`gte_new`] that [`gte_drop`] has not yet been called on.
  */
 void gte_reserve(struct DbGte *gte, uint32_t *n_vars_used);
+
+/**
+ * Frees the memory associated with a [`DbGte`]
+ *
+ * # Safety
+ *
+ * `gte` must be a return value of [`gte_new`] and cannot be used
+ * afterwards again.
+ */
+void gte_drop(struct DbGte *gte);
+
+/**
+ * Creates a new [`DbTotalizer`] cardinality encoding
+ */
+struct DbTotalizer *tot_new(void);
 
 /**
  * Adds a new input literal to a [`DbTotalizer`]
@@ -534,15 +539,6 @@ void gte_reserve(struct DbGte *gte, uint32_t *n_vars_used);
  * If the passed `lit` is an invalid IPASIR literal
  */
 enum MaybeError tot_add(struct DbTotalizer *tot, int lit);
-
-/**
- * Frees the memory associated with a [`DbTotalizer`]
- *
- * # Safety
- *
- * `tot` must be a return value of [`tot_new`] and cannot be used afterwards again.
- */
-void tot_drop(struct DbTotalizer *tot);
 
 /**
  * Lazily builds the _change in_ cardinality encoding to enable upper bounds in a given range. A
@@ -586,11 +582,6 @@ void tot_encode_ub(struct DbTotalizer *tot,
 enum MaybeError tot_enforce_ub(struct DbTotalizer *tot, size_t ub, int *assump);
 
 /**
- * Creates a new [`DbTotalizer`] cardinality encoding
- */
-struct DbTotalizer *tot_new(void);
-
-/**
  * Reserves all auxiliary variables that the encoding might need
  *
  * All calls to [`tot_encode_ub`] following a call to this function are guaranteed to not increase
@@ -601,6 +592,15 @@ struct DbTotalizer *tot_new(void);
  * `tot` must be a return value of [`tot_new`] that [`tot_drop`] has not yet been called on.
  */
 void tot_reserve(struct DbTotalizer *tot, uint32_t *n_vars_used);
+
+/**
+ * Frees the memory associated with a [`DbTotalizer`]
+ *
+ * # Safety
+ *
+ * `tot` must be a return value of [`tot_new`] and cannot be used afterwards again.
+ */
+void tot_drop(struct DbTotalizer *tot);
 
 #ifdef __cplusplus
 }  // extern "C"
