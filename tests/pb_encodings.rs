d0670ff45c54747988ba75d9ce6bc28474fe21a9
Christoph Jabs <christoph.jabs@helsinki.fi>
2025-11-11 13:00:31 +0200

    feat(pigeons): permutation and constraints output types

--- tests/pb_encodings.rs
+++ tests/pb_encodings.rs
@@ -792,7 +792,7 @@ mod cert {
         assert_eq!(res, SolverResult::Sat);
 
         let proof_file = proof
-            .conclude::<Var>(pigeons::OutputGuarantee::None, &pigeons::Conclusion::None)
+            .conclude::<Var>(&pigeons::OutputGuarantee::None, &pigeons::Conclusion::None)
             .unwrap();
         let manifest = std::env::var("CARGO_MANIFEST_DIR").unwrap();
         verify_proof(format!("{manifest}/data/empty.opb"), proof_file.path());
@@ -861,7 +861,7 @@ mod cert {
         }
 
         let proof_file = proof
-            .conclude::<Var>(pigeons::OutputGuarantee::None, &pigeons::Conclusion::None)
+            .conclude::<Var>(&pigeons::OutputGuarantee::None, &pigeons::Conclusion::None)
             .unwrap();
         let manifest = std::env::var("CARGO_MANIFEST_DIR").unwrap();
         verify_proof(format!("{manifest}/data/empty.opb"), proof_file.path());
